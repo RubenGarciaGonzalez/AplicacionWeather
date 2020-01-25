dd03a468c82598f79728b7c7b44c6bd48065097f
RubenGarciaGonzalez <rubengg_00@hotmail.com>
2020-01-25 11:51:49 +0100

    Segundo push aplicacion tiempo

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 window.addEventListener('load', ()=>{
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position =>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const api = `https://api.darksky.net/forecast/6f885ab6612d61643ba4dd58dcfb8984/${lat},${long}`;
-        })   
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/6f885ab6612d61643ba4dd58dcfb8984/${lat},${long}`;
+
+            fetch(api)
+            .then(response =>{
+                return response.json();
+            })
+            .then(data =>{
+                const {temperature, summary, icon} = data.currently;
+                //Set DOM Elements from the API
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+                    //Set Icon
+                    setIcons(icon, document.querySelector('.icon'));
+            });
+        });
     }
+
+    function setIcons(icon,iconID){
+        const skycons = new skycons({color:"white"});
+        const curretnIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[curretnIcon]);
+    }
+
 });
